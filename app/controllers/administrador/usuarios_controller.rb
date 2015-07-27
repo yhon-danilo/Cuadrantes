f89cf35ea90462e1a06b84e353744b4dd9e0ad83
Yhon Danilo Tabares Ramirez <yhondanilo@gmail.com>
2015-07-26 22:03:42 -0500

    configuracion administrador

---
+++ app/controllers/administrador/usuarios_controller.rb
@@ -0,0 +1,22 @@
+class Administrador::UsuariosController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def parametros_usuarios
+  end
+end
