f7f2d29eb2db005b038165d86d38aeae346e6402
Yhon Danilo Tabares Ramirez <yhondanilo@gmail.com>
2015-08-03 02:56:09 -0500

    se agregan validadciones para usuario

--- app/controllers/administrador/usuarios_controller.rb
+++ app/controllers/administrador/usuarios_controller.rb
@@ -1,22 +1,47 @@
 class Administrador::UsuariosController < ApplicationController
+  before_action :set_usuario, :only => [:show, :edit, :update, :destroy]
+
   def index
+
   end
 
   def new
+    @usuario = Usuario.new
   end
 
   def create
+    @usuario = Usuario.new(parametros_usuario)
+    if @usuario.save
+      redirect_to @usuario
+    else
+      render 'new'
+    end  
+
+  end
+
+  def show
+    
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+
   end
 
-  def parametros_usuarios
+
+  private
+  def set_usuario
+    @usuario = Usuario.find(params[:id])
+  end
+
+  def parametros_usuario
+    params.require(:usuario).permit(:nombre_usuario, :password, :nombre, :apellido)
   end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,28 @@
 class Usuario < ActiveRecord::Base
+  attr_accessor :password_confirmation
+  validates :nombre_usuario, :presence => :true, :uniqueness => true
+  before_save :encrypt_password
+
+
+
+  def encrypt_password
+    if password.present?
+	  self.salt=BCrypt::Engine.generate_salt
+	  self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+	end
+  end 
+
+  def self.authenticate(username="", login_password="")
+    user = Usuario.find_by_nombre_usuario(username)
+    if user && user.match_password(login_password)
+      return user
+    else
+      return false
+    end
+  end 
+
+  def match_password(login_password="")
+    encrypted_password == BCrypt::Engine.hash_secret(login_password, salt)
+  end
+
 end
